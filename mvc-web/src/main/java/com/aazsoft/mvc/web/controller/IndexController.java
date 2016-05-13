5b0e9738f523535b887fb959e0625791776ad0f6
Le Quoc Truong <truong.le@swissitbridge.com>
2016-05-13 09:38:56 +0700

    correct label remember me
    Change-Id: I2a22d97ee7f3d4cb7bf8ead1b9d315aa5f64ab00

--- mvc-web/src/main/java/com/aazsoft/mvc/web/controller/IndexController.java
+++ mvc-web/src/main/java/com/aazsoft/mvc/web/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.aazsoft.mvc.web.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Model model) {
+	public String index() {
 		return "index";
 	}
 
