ab3a369c9d0605e85a2f2fbec5ae2adbbe51d44b
Le Quoc Truong <truong.le@swissitbridge.com>
2016-05-17 18:03:13 +0700

    Draft: update configuration for mysql

--- mvc-dao/src/main/java/com/aazsoft/mvc/dao/entity/User.java
+++ mvc-dao/src/main/java/com/aazsoft/mvc/dao/entity/User.java
@@ -11,61 +11,59 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "user")
 public class User implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, updatable = false)
-    private Long id;
+	private static final long serialVersionUID = 3293806819349409124L;
 
-    @Column(name = "email", nullable = false, unique = true)
-    private String email;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false, updatable = false)
+	private Long id;
 
-    @Column(name = "password_hash", nullable = false)
-    private String passwordHash;
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
 
-    @Column(name = "role", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Role role;
+	@Column(name = "password_hash", nullable = false)
+	private String passwordHash;
 
-    public Long getId() {
-        return id;
-    }
+	@Column(name = "role", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
-    public String getEmail() {
-        return email;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getPasswordHash() {
-        return passwordHash;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
+	public String getPasswordHash() {
+		return passwordHash;
+	}
 
-    public Role getRole() {
-        return role;
-    }
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
 
-    public void setRole(Role role) {
-        this.role = role;
-    }
+	public Role getRole() {
+		return role;
+	}
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email.replaceFirst("@.*", "@***") +
-                ", passwordHash='" + passwordHash.substring(0, 10) +
-                ", role=" + role +
-                '}';
-    }
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" + "id=" + id + ", email='"
+				+ email.replaceFirst("@.*", "@***") + ", passwordHash='"
+				+ passwordHash.substring(0, 10) + ", role=" + role + '}';
+	}
 }
