03f241a269fc356af6837f15357d4ba6295e3b7d
Le Quoc Truong <truong.le@swissitbridge.com>
2016-05-25 11:58:59 +0700

    enhance security
    Change-Id: I19389d864c880a8ead989c73d944fde3b8267eb4

--- mvc-security/src/main/java/com/aazsoft/mvc/security/config/SecurityConfiguration.java
+++ mvc-security/src/main/java/com/aazsoft/mvc/security/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.aazsoft.mvc.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,7 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-@PropertySource("classpath:application-service.properties")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -24,7 +22,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/", "/public/**", "/webjars/**", "/**").permitAll()
+				.antMatchers("/", "/public/**", "/webjars/**").permitAll()
 				.antMatchers("/users/**").hasAuthority("ADMIN")
 				.anyRequest().fullyAuthenticated()
 				.and().formLogin().loginPage("/login").failureUrl("/login?error")
